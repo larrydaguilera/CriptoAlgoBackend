ce02f4602e5eb4466b124c0fe10d8b8668670c5c
larrydaguilera <55728194+larrydaguilera@users.noreply.github.com>
2022-10-21 00:16:27 -0300

    Added url to security config

--- CriptoAlgo/src/main/java/com/C722/CriptoAlgo/criptoAlgo/auth/security/SecurityConfig.java
+++ CriptoAlgo/src/main/java/com/C722/CriptoAlgo/criptoAlgo/auth/security/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://iwill88.github.io"));
         configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "DELETE", "POST", "PATCH", "HEAD", "TRACE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         //configuration.setAllowCredentials(true);
